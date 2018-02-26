1ccda01035f77367e5643b935997b2cf5e44334d
eyqin93_kaklong@yahoo.com <User@LAB409>
2018-02-26 14:04:42 +0800

    My AddNumber

---
+++ AddNumber/src/AddNumber.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class AddNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println(" My Name is Shikin");
+	
+	int x=10;
+	int y=10;
+	System.out.println("Number 1 = " + x );
+	System.out.println("Number 2 = " + y);
+	int sum = x + y ;
+	
+	System.out.println("Sum = "+ sum   );
+	}
+
+}
